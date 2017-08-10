8a4c8ba76c55ab165d7fc538a81e0f7802de32ac
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-08-10 17:51:47 +0100

    Refactor code to add updated_at

--- tests/app/test_model.py
+++ tests/app/test_model.py
@@ -19,7 +19,7 @@ from tests.app.conftest import (
     sample_template as create_sample_template,
     sample_notification_with_job as create_sample_notification_with_job
 )
-from tests.app.db import create_notification
+from tests.app.db import create_notification, create_service, create_inbound_number
 
 
 @pytest.mark.parametrize('mobile_number', [
@@ -203,3 +203,12 @@ def test_letter_notification_serializes_with_address(client, sample_letter_notif
     assert res['line_5'] is None
     assert res['line_6'] is None
     assert res['postcode'] == 'SW1 1AA'
+
+
+def test_inbound_number_serializes_with_service(client, notify_db_session):
+    service = create_service()
+    inbound_number = create_inbound_number(number='1', service_id=service.id)
+    serialized_inbound_number = inbound_number.serialize()
+    assert serialized_inbound_number.get('id') == str(inbound_number.id)
+    assert serialized_inbound_number.get('service').get('id') == str(inbound_number.service.id)
+    assert serialized_inbound_number.get('service').get('name') == inbound_number.service.name
